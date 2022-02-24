0d0634d0581bf0d6a9e607bd640fa9babf6f3529
duwilaja <apaaja.software@gmail.com>
2022-02-24 10:46:51 +0700

    jenisinfo -> lov

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -204,6 +204,7 @@ class Laporan extends CI_Controller {
 			}
 			if($id=='tmc_interaksi'||$id=='tmc_publikasi'){  //
 				$data['media'] = ($this->db->select('val,txt')->where('grp','media')->get('lov')->result_array());
+				$data['jenisinfo'] = ($this->db->select('val,txt')->where('grp','jenisinfo')->get('lov')->result_array());
 			}
 			
 			if($id=='tmc_info_lalin' || $id=='ais_laka' || $id=='tmc_ops_macet' || $id=='tmc_ops_pol'){  //tmc info lalin
